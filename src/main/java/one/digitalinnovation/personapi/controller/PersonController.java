5d4df5889f717ce7c592057e4541ab525f8ece86
marcelodomingosneto <marcelodomingosneto@gmail.com>
2021-09-08 20:26:57 -0300

    v7

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -43,6 +43,11 @@ public class PersonController {
             return personService.findByid(id);
         }
 
+       @PutMapping("/{id}")
+        public MessageResponseDTO updateById(@PathVariable Long id, @RequestBody @Valid PersonDTO personDTO) throws PersonNotFoundException {
+            return personService.updateById(id, personDTO);
+        }
+
 
         @DeleteMapping("/{id}")
         @ResponseStatus(HttpStatus.NO_CONTENT)
